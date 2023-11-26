7a5a1db3e985849ea865c25de6e3b9a84a752351
TopBoyDrako <sinbadprince9@gmail.com>
2023-11-26 23:07:07 +0100

    tic-tac-toe update

--- game_time.py
+++ game_time.py
@@ -1,60 +1,70 @@
 #!/usr/bin/python3
 
-"""
-This module defines a project about a tic-tac-toe game.
-It allows 2 users to play the game
-"""
-from IPython.display import clear_output
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
 
-def display_board(board):
-    clear_output()  # Remember, this only works in jupyter!
-    
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
+def check_winner(board):
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != ' ':
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != ' ':
+            return board[0][i]
 
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
 
-def player_input():
-    marker = ''
-    
-    while not (marker == 'X' or marker == 'O'):
-        marker = input('Player 1: Do you want to be X or O? ').upper()
+    return None
 
-    if marker == 'X':
-        return ('X', 'O')
-    else:
-        return ('O', 'X')
+def is_board_full(board):
+    for row in board:
+        if ' ' in row:
+            return False
+    return True
 
-def place_marker(board, marker, position):
-    board[position] = marker
+def tic_tac_toe():
+    # Initialize the board
+    board = [[' ' for _ in range(3)] for _ in range(3)]
 
-def win_check(board,mark):
-    
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
-    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
-    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
-    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
-    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
-    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
-    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
-    (board[9] == mark and board[5] == mark and board[1] == mark))
+    # Initialize the player
+    current_player = 'X'
 
-import random
+    # Game loop
+    while True:
+        print_board(board)
 
-def choose_first():
-    if random.randint(0, 1) == 0:
-        return 'Player 2'
-    else:
-        return 'Player 1
+        # Player move
+        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
+        col = int(input(f"Player {current_player}, enter column (0, 1, or 2): "))
 
-def space_check(board, position):
-    
-    return board[position] == ' '
+        # Check if the chosen cell is empty
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+        else:
+            print("Cell already taken. Try again.")
+            continue
+
+        # Check for a winner
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            print(f"Player {winner} wins!")
+            break
+
+        # Check for a tie
+        if is_board_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        # Switch player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    tic_tac_toe()
+
